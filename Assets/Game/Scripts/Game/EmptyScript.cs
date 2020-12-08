8ec7a02bfea47d8ae9cbd0a94e1ee2e7549b991f
Jesús González <jesus@jesusmg.net>
2020-12-08 13:39:53 -0300

    Initial commit.

---
+++ Assets/Game/Scripts/Game/EmptyScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class EmptyScript : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
